a2fe1c3f360b831ee1688ebcf4bd25e2c86ab7f0
Emrullah <emr.ozk.71@gmail.com>
2022-06-03 21:09:08 +0300

    First commit

---
+++ src/main/java/com/example/jspwebapp/JspWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.jspwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspWebAppApplication.class, args);
+    }
+
+}
